b03ca0819f7b7c3f419b29f39743fadd398a8af2
phoEng4 <56479909+phoEng4@users.noreply.github.com>
2020-04-17 17:54:58 +0630

    Update disinfectionChamber.ino

--- disinfectionChamber.ino
+++ disinfectionChamber.ino
@@ -1,3 +1,5 @@
+//created by 4N
+
 #define ldrPin A2
 #define ledGreenPin 7//relay input 4
 #define ledRedPin 6//relay input 3
@@ -15,9 +17,11 @@ void setup() {
 }
 
 void loop() {
+  //read the ldr sensor and insert the ldrval variable
   ldrVal = analogRead(ldrPin);
   Serial.print("ldr Val : ");
   Serial.println(ldrVal);
+  //If ldrval variable less than 51, working the output devices but not working the green led.
   if(ldrVal <= 50 ){
     digitalWrite(buzzerPin,HIGH);
     delay(1000);

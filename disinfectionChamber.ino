72c8a8c7b5757abedd893dfd8bdb80fef4d2d5f6
phoEng4 <56479909+phoEng4@users.noreply.github.com>
2020-04-17 17:49:17 +0630

    Add files via upload

---
+++ disinfectionChamber.ino
@@ -0,0 +1,46 @@
+#define ldrPin A2
+#define ledGreenPin 7//relay input 4
+#define ledRedPin 6//relay input 3
+#define buzzerPin 5//relay input 2
+#define pumpPin 8 //relay input 1
+int ldrVal ;
+
+void setup() {
+  pinMode(ldrPin,INPUT);
+  pinMode(ledGreenPin,OUTPUT);
+  pinMode(ledRedPin,OUTPUT);
+  pinMode(buzzerPin,OUTPUT);
+  pinMode(pumpPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  ldrVal = analogRead(ldrPin);
+  Serial.print("ldr Val : ");
+  Serial.println(ldrVal);
+  if(ldrVal <= 50 ){
+    digitalWrite(buzzerPin,HIGH);
+    delay(1000);
+    digitalWrite(buzzerPin,LOW);
+    Serial.println("Buzzer is work.");
+    digitalWrite(pumpPin,HIGH);
+    digitalWrite(ledRedPin,HIGH);
+    digitalWrite(ledGreenPin,LOW);
+    Serial.println("pump is working");
+    delay(5000);
+    digitalWrite(pumpPin,LOW);
+    Serial.println("humans is out.");
+    delay(1000);
+    digitalWrite(pumpPin,HIGH);
+    delay(500);
+    digitalWrite(pumpPin,LOW);
+    digitalWrite(ledRedPin,LOW);
+  }
+  else{
+     digitalWrite(buzzerPin,LOW);
+    digitalWrite(pumpPin,LOW);
+    digitalWrite(ledRedPin,LOW);
+    digitalWrite(ledGreenPin,HIGH);
+    Serial.println("pump is not working");
+  }
+}
